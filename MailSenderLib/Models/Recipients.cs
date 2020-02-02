f61b623b11cb678b6fa468a0ba30fefd373be167
Dmitry Bandurkin <dmitrybandurkin@gmail.com>
2020-02-02 18:15:30 +0300

    Добавлена библиотека с моделями

---
+++ MailSenderLib/Models/Recipients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSenderLib.Models
+{
+    class Recipients
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
