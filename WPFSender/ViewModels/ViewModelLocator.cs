359e1bf200c6f07c5d96c5020632941303e76f0d
Dmitry Bandurkin <dmitrybandurkin@gmail.com>
2020-02-08 14:12:48 +0300

    реализация MVVM

---
+++ WPFSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace WPFSender.ViewModels
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+        }
+    }
+}
