12b7ae26232230af12189f86825f5765487db446
Dmitry Bandurkin <dmitrybandurkin@gmail.com>
2020-02-15 17:15:51 +0300

    Параллельный факториал

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TestConsole
 {
@@ -6,7 +7,38 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var thread_one = new Thread(MyThreadClass.FactorialCalculating);
+            var thread_two = new Thread(MyThreadClass.SumCalculating);
+
+            Console.WriteLine("Число для факториала?");
+            MyThreadClass.NumF = int.Parse(Console.ReadLine());
+            thread_one.Start();
+            Console.WriteLine("Число для суммы?");
+            MyThreadClass.NumS = int.Parse(Console.ReadLine());
+            thread_two.Start();
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class MyThreadClass
+    {
+        public static int NumF { get; set; }
+        public static int NumS { get; set; }
+
+        public static void FactorialCalculating() => Console.WriteLine($"Факториал: {Factorial(NumF)}");
+        public static void SumCalculating() => Console.WriteLine($"Сумма: {Sum(NumS)}");
+        static int Factorial(int i)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine($"Идет вычисление факториала");
+            return (i == 1) ? 1 : i * Factorial(i - 1);
+        }
+        static int Sum(int n)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine($"Идет вычисление суммы");
+            return (n == 0) ? 0 : n + Sum(n - 1);
         }
     }
 }
